e14ec7e97d39062362c87659fcd7c61211d51fd5
James Traver <jtraver@gogoair.com>
2018-12-21 00:38:36 -0600

    ok cleanup and bed

--- src/main/scala/org/codeforcoffee/models/Planet.scala
+++ src/main/scala/org/codeforcoffee/models/Planet.scala
@@ -2,6 +2,7 @@ package org.codeforcoffee.models
 
 /**
   * Planet
+  *
   * @param starHostname
   * @param planetLetter
   * @param orbitalPeriod
@@ -9,4 +10,4 @@ package org.codeforcoffee.models
   * @param radius
   * @param category
   */
-case class Planet(starHostname: String, planetLetter: String, orbitalPeriod: BigDecimal, mass: BigDecimal, radius: BigDecimal, category: String)
+case class Planet(id: Int, starHostname: String, planetLetter: String, orbitalPeriod: BigDecimal, mass: BigDecimal, radius: BigDecimal)
